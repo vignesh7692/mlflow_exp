983b088841a61b7e09f7e5d36ade3331d4a96345
Vignesh Selvaraj <vignesh.selvaraj@freshworks.com>
2022-11-14 11:51:54 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import mlflow
+
+def print_delta():
+    data = sqlContext.sql("select * from freddy_fm_d2c.d2c_accounts where mcr_account_id = 1511679284073906176 ").toPandas()
+    accid = data['mcr_account_id']
+    mlflow.log_param("acc_id", accid)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    log_delta()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
